086394c530ca261f43cf9e73815fafad0a55fd7a
ptbelo <ptbelo@hotmail.com>
2021-12-21 16:40:44 -0300

    commitando com erro

---
+++ api/index.js
@@ -0,0 +1,12 @@
+// pra iniciar o node a gente dá um "npm init -y"
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
+
+app.use(bodyParser.json())
+
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
